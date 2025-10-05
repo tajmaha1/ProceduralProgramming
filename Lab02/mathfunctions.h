97eb7d4c0b1033f82310b379460c34e0c012d84e
megatr4n <d.cherniavskyi@ukma.edu.ua>
2025-10-05 17:06:34 +0300

    Add files via upload

---
+++ Lab02/mathfunctions.h
@@ -0,0 +1,13 @@
+#ifndef MATHFUNCTIONS_H
+#define MATHFUNCTIONS_H
+
+extern const double PI;
+extern const double EPSILON;
+
+double normalizeAngle(double x);
+double mySin(double x);
+double myCos(double x);
+double myTan(double x);
+double myExp(double x);
+
+#endif
---
+++ Lab02/testing.h
@@ -0,0 +1,7 @@
+#ifndef TESTING_H
+#define TESTING_H
+
+void printResult(const char* funcName, double x, double myResult, double stdResult);
+void runTests();
+
+#endif
