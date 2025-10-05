0df623f17385f70fe4e5cd91fe9b279df719e841
megatr4n <d.cherniavskyi@ukma.edu.ua>
2025-10-05 18:32:34 +0300

    Delete Lab02 directory

--- Lab02/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "mathfunctions.h"
-#include "testing.h"
-
-int main() {
-    runTests();
-    return 0;
-}
--- Lab02/testing.h
+++
@@ -1,7 +0,0 @@
-#ifndef TESTING_H
-#define TESTING_H
-
-void printResult(const char* funcName, double x, double myResult, double stdResult);
-void runTests();
-
-#endif
